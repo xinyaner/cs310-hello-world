be32df59a64418990d047f9e2d815f65ca4b74cb
Jay Snellen, III <jsnellen@jsu.edu>
2018-08-27 09:29:55 -0500

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Your Name
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Class c1 = new Class1();
+        Class c2 = new Class2();
+        System.out.println("Hello, World!");
+    }
+    private void f1() {
+        // v1.0
+    }
+
+}
